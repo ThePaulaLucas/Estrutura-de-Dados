53b84438929323e3c4682b487ea63eb3b8c881dc
Maria Eduarda Hisse <43155056+mariahisse@users.noreply.github.com>
2018-10-30 21:53:37 -0300

    Add files via upload

---
+++ Trabalhos/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include "gtest/gtest.h"
+#include "imatriz.cpp"
+#include "teste.h"
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+
+    return 0;
+}
+
